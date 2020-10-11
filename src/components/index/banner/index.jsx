cdb19183e99e394c88aad13da90c4eaaff419c85
idcunyu <695035427@qq.com>
2020-10-11 19:47:33 +0800

    fix: style

--- src/components/index/banner/index.jsx
+++ src/components/index/banner/index.jsx
@@ -16,16 +16,16 @@ class IndexBanner extends Component {
     return (
       <div className="index-banner">
         <div className="index-banner-all">
-          <img src="/assets/images/index/slogan.png" alt="slogan" className="slogan wow swing"/>
-          <div className="desc wow bounceInLeft">
+          <img src="/assets/images/index/slogan.png" alt="slogan" className="slogan wow fadeInDown"/>
+          <div className="desc wow fadeInDown">
             Unprecedented mining experience, easily to get <br/>
             benefits from the blockchain.
           </div>
-          <div className="btns wow lightSpeedIn">
+          <div className="btns wow fadeInDown">
             <a href="/fund" className="btn left-btn">FUND</a>
             <a href="/whitepaper" className="btn right-btn">WHITEPAPER</a>
           </div>
-          <div className="more wow bounceInLeft">
+          <div className="more wow fadeInDown">
             <img src="/assets/images/index/promo.png" alt="promo" className="promo"/>
             <span className="content">S.E.E.D network has started,Get UMI Token now!</span>
             <a href="/detail" className="detail">Details</a>
